3961f6a003afc08d8aa65b6fecca55a2c158536e
david <dgallula@gmail.com>
2022-07-05 19:11:03 +0300

    initial commit

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,46 @@
+import Proj1 from "../assets/proj1.jpg";
+import Proj2 from "../assets/proj2.jpg";
+import Proj3 from "../assets/proj3.webp";
+import Proj4 from "../assets/proj4.webp";
+import Proj5 from "../assets/proj5.jpg";
+import Proj6 from "../assets/proj6.png";
+
+
+
+
+export const ProjectList = [
+  {
+    name: "Task to do in JavaScript,HTML,CSS",
+    image: Proj1,
+    skills: " project-01-john-bryce.vercel.app ",
+  },
+  {
+    name: "Ecommerce Website",
+    image: Proj2,
+    skills: "Angular,Node.js,MongoDB",
+  },
+  {
+    name: "api movies React,Node.js,API,AXIOs",
+    image: Proj3,
+    skills:   "http://react-movies-cb8d9cfc43070c00f6c5fe7dc04310d2dfa9ae41.vercel.app/",
+  },
+  {
+    name: "Social Media Website",
+    image: Proj4,
+    skills: "React,Node.js,MySQL,GraphQL",
+  },
+  {
+    name: "Bitcoins in Javascript , html ,css, API fetch",
+    image: Proj5,
+    skills: "http://projet2-indol.vercel.app/",
+  },
+  {
+    name: "Memory  Game",
+    image: Proj6,
+    skills: "http://memory-game-in-javascript.vercel.app/"
+  },
+  
+];
+
+
+
---
+++ src/pages/Experience.js
@@ -0,0 +1,80 @@
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import SchoolIcon from "@material-ui/icons/School";
+import WorkIcon from "@material-ui/icons/Work";
+
+function Experience() {
+  return (
+    <div className="experience">
+      <VerticalTimeline lineColor="#3e497a">
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2000 - 2001"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            My  High School, Baccalaureat Mathematiques , Mathematiques -2000 
+          </h3>
+          <p> High School Diploma</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2014 - 2018"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Rabbinic School , juin2006 , rabbinical Degree 
+          </h3>
+
+          <h4 className="vertical-timeline-element-subtitle">
+          </h4>
+
+          <p> </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2019 - 2022"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Study school -Paris Full Paris 2019-2020 - Cours Online with degree 
+             John Bryce Full stack Web application -Jerusalem 2021-2022 
+
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            
+          </h4>
+          <p>Developed   4 projects at John Bryce and 5 tests .</p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2021 - present"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+             Formel - Listeners admin in marketing 
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            
+          </h4>
+          <p>
+             
+          </p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
+}
+
+export default Experience;
+
+
